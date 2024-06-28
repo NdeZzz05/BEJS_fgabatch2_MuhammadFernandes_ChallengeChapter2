ce2054d68fd2e1484f958c850954fca3a281d3f6
NdeZzz05 <muhammadfernandes05@gmail.com>
2024-06-28 14:07:10 +0700

    Initialize commit

---
+++ banking_system.js
@@ -0,0 +1,80 @@
+class ValidationError extends Error {
+  constructor(param, message) {
+    super(message);
+    this.param = param;
+  }
+}
+
+class BankAccount {
+  #balance = 0;
+
+  deposit(amount) {
+    return new Promise((resolve, reject) => {
+      if (isNaN(amount) || amount <= 0) {
+        reject(new ValidationError("Deposit", "Invalid amount"));
+      } else {
+        alert(`You have deposited ${amount} into your account`);
+        setTimeout(() => {
+          this.#balance += amount;
+          console.log(`You have total balance: ${this.#balance}`);
+          resolve(this.#balance);
+        }, 1000);
+      }
+    });
+  }
+
+  withdraw(amount) {
+    return new Promise((resolve, reject) => {
+      if (isNaN(amount) || amount <= 0) {
+        reject(new ValidationError("Withdraw", "Invalid amount"));
+      } else if (amount > this.#balance) {
+        reject(new ValidationError("Withdraw", "Insufficient funds"));
+      } else {
+        alert(`You have withdrawn ${amount} from your account`);
+        setTimeout(() => {
+          this.#balance -= amount;
+          console.log(`You have total balance: ${this.#balance}`);
+          resolve(this.#balance);
+        }, 1000);
+      }
+    });
+  }
+
+  transfer(amount, targetAccount) {
+    if (isNaN(amount) || amount <= 0) throw new ValidationError("Transfer", "Invalid amount");
+    return new Promise(async (resolve, reject) => {
+      if (targetAccount instanceof BankAccount) {
+        try {
+          await this.withdraw(amount);
+          await targetAccount.deposit(amount);
+          console.log(`Successfully transferred ${amount} to target account`);
+        } catch (error) {
+          reject(error);
+        }
+      } else {
+        reject(new ValidationError("Transfer", "Invalid target account"));
+      }
+    });
+  }
+
+  get balance() {
+    return this.#balance;
+  }
+}
+
+const account1 = new BankAccount();
+const account2 = new BankAccount();
+
+const run = async () => {
+  try {
+    await account1.deposit(10000);
+    await account1.transfer(10000, account2);
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      console.log(`${error.param}: ${error.message}`);
+    } else {
+      console.log(error.message);
+    }
+  }
+};
+run();
